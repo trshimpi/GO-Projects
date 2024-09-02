907975d1aa35129c73e212f262a39366c65bf981
tanmay-kreeda <tanmay.shimpi@kreedalabs.com>
2024-09-02 22:53:26 +0530

    Refractored handlers main and render files and added bootstrap

---
+++ Hello World/render.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// renderTemplate renders a template
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+}

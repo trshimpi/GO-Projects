7d3425524cb178a2593ce461d16495717ae9d383
tanmay-kreeda <tanmay.shimpi@kreedalabs.com>
2024-09-02 23:32:56 +0530

    Added base layout

--- Hello World/pkg/render/render.go
+++ Hello World/pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // renderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
